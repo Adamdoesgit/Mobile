dc6147feb92bc230af6b7503f546dab4e8b3e7e3
AlexanderBulanov <alexanderbulanovcodes@gmail.com>
2016-01-14 09:34:44 -0800

    Update Branch.java

--- Branch.java
+++ Branch.java
@@ -2,10 +2,15 @@
 
 public class Branch {
 	public double length;
-	
+	public Structure structure;
 	
 	public Branch(double length, Structure structure) {
-		
+		this.length = length;
+		this.structure = structure;
+	}
+	
+	public Structure getStructure() {
+		return structure;
 	}
 	
 }
