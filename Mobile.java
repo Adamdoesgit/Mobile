669f75a6a28ab986b14a8b11b72a8366af947713
AlexanderBulanov <alexanderbulanovcodes@gmail.com>
2016-01-14 09:34:27 -0800

    Update Mobile.java

--- Mobile.java
+++ Mobile.java
@@ -1,13 +1,32 @@
 //Alexander Bulanov, Adam Barszczak
 
-public class Mobile extends Structure {
+public class Mobile implements Structure {
+	public Branch left;
+	public Branch right;
 
 	public static void main(String[] args) {
 
 	}
-	
-	public Mobile(Branch LeftBranch, Branch RightBranch) {
-		
+
+	public Mobile(Branch left, Branch right) {
+		this.left = left;
+		this.right = right;
 	}
 
+	public double getWeight() {
+		return (left.
+	}
+
+	public boolean isBalanced() {
+
+	}
+	
+	public String toString() { //May not work
+		return " " + mob;
+	}
+
+	public Mobile mob = new Mobile(lb, rb);
+	public Branch lb = new Branch(5, lw);
+	public Branch rb = new Branch(5, rw);
+	
 }
